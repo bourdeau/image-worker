526a4f10d8a394c6cafde39b18f8384ff7b0f4f3
Pierre-Henri <phbasic@gmail.com>
2017-01-13 15:27:01 +0100

    A bit of refactoring

--- worker.py
+++ worker.py
@@ -8,10 +8,9 @@ from multiprocessing import Pool, Lock
 class Worker:
 
     def __init__(self):
-        self.debug = False
         self.poolsize = 8
-        self.cdnPath = "/home/ph/Bureau/cdn_squarebreak"
-        self.cdnNewPath = "/home/ph/Bureau/PYTHON_cdn_squarebreak"
+        self.sourceDir = "/home/ph/Bureau/cdn_squarebreak"
+        self.destinationPath = "/home/ph/Bureau/cdn2_squarebreak"
         self.quality = 80
         self.sizes = [
             250,
@@ -22,25 +21,16 @@ class Worker:
             300,
         ]
 
-    def main(self):
-        images = self.getImages()
-        self.multiplex(images)
-
     """
     Run resize in Multiprocessing
     """
-    def multiplex(self, images):
+    def main(self):
         pool = Pool(processes=self.poolsize)
-        nb = 0
+        images = self.getImages()
         for size in self.sizes:
             for image in images:
                 try:
-                    if self.debug:
-                        debug = pool.apply_async(self.resize, (image, size, nb))
-                        debug.get()
-                    else:
-                        pool.apply_async(self.resize, (image, size, nb))
-                    nb += 1
+                    pool.apply_async(self.resize, (image, size))
                 except OSError as e:
                     print(e)
         pool.close()
@@ -51,7 +41,7 @@ class Worker:
     """
     def getImages(self):
         originals = []
-        images = glob.glob(self.cdnPath+'/**/*.jpg', recursive=True)
+        images = glob.glob(self.sourceDir+'/**/*.jpg', recursive=True)
         for image in images:
             match = re.search('\/([A-Za-z0-9]*)\.jpg', image)
             if (match):
@@ -65,7 +55,7 @@ class Worker:
     """
     Resize the image and save it
     """
-    def resize(self, imagePath, size, nb):
+    def resize(self, imagePath, size):
         try:
             imageLib = Image.open(imagePath).convert('RGB')
             # Bug https://github.com/python-pillow/Pillow/issues/1237
@@ -83,7 +73,7 @@ class Worker:
 
             im2 = image.resize((width, height), Image.ANTIALIAS)
             image.close()
-            newPath = self.cdnNewPath+'/'+str(size)
+            newPath = self.destinationPath+'/'+str(size)
             # Create directory with appropriate size
             if not os.path.exists(newPath):
                 os.makedirs(newPath)
