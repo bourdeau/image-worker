0f8537ab4c9b694806432f5dd1968a1e0f4f1a76
Pierre-Henri <phbasic@gmail.com>
2017-01-15 19:30:43 +0100

    Refactoring

--- tests/imageworker/worker.py
+++ tests/imageworker/worker.py
@@ -5,33 +5,34 @@ from imageworker.worker import Worker
 
 
 class WorkerTest(unittest.TestCase):
-
+    sourceDir = './tests/imageworker/images-source'
+    destDir = './tests/imageworker/images-dest'
     def test_invalid_directories(self):
         test = Worker('/idonotexisthopefully', '/donotexisteither', -80, False, [100])
         self.assertRaises(Exception, test.main)
 
     def test_invalid_quality(self):
-        test = Worker('./tests/imageworker/images-source', './tests/imageworker/images-dest', -1, False, [300])
+        test = Worker(self.sourceDir, self.destDir, -1, False, [300])
         self.assertRaises(Exception, test.main)
-        test = Worker('./tests/imageworker/images-source', './tests/imageworker/images-dest', 101, False, [400])
+        test = Worker(self.sourceDir, self.destDir, 101, False, [400])
         self.assertRaises(Exception, test.main)
 
     def test_invalid_dimensions(self):
-        test = Worker('./tests/imageworker/images-source', './tests/imageworker/images-dest', 80, False, 'adazd')
+        test = Worker(self.sourceDir, self.destDir, 80, False, 'adazd')
         self.assertRaises(Exception, test.main)
-        test = Worker('./tests/imageworker/images-source', './tests/imageworker/images-dest', 80, False, [7894949])
+        test = Worker(self.sourceDir, self.destDir, 80, False, [7894949])
         self.assertRaises(Exception, test.main)
 
     def test_invalid_resizing(self):
-        test = Worker('./tests/imageworker/images-source', './tests/imageworker/images-dest', 80, False, [600])
+        test = Worker(self.sourceDir, self.destDir, 80, False, [600])
         test.main()
-        if not os.path.isfile('./tests/imageworker/images-dest/originals/rabbit.jpg'):
+        if not os.path.isfile(self.destDir+'/originals/rabbit.jpg'):
             raise Exception('Copying original file failed!')
-        if not os.path.isfile('./tests/imageworker/images-dest/600/rabbit.jpg'):
+        if not os.path.isfile(self.destDir+'/600/rabbit.jpg'):
             raise Exception('Copying original file failed!')
 
-        for the_file in os.listdir('./tests/imageworker/images-dest'):
-            shutil.rmtree('./tests/imageworker/images-dest/' + the_file)
+        for the_file in os.listdir(self.destDir):
+            shutil.rmtree(self.destDir+'/' + the_file)
 
 if __name__ == '__main__':
     unittest.main()
